874c95728ca02bb86660649cb60d10154fe602c7
wypoch <wywyt213@gmail.com>
2026-04-17 14:26:28 -0600

    Added sleeps

--- signature_replay/entrypoint.sh
+++ signature_replay/entrypoint.sh
@@ -8,6 +8,15 @@ gcc replay.c -o replay \
     -I/ION-DTN/ici/include \
     -L/usr/local/lib -lams -lici -ldgr
 
+echo "Starting ION..."
 ionadmin amroc.ionrc
+sleep 1
+
+echo "Starting AMS..."
 amsd @ @ amsdemo test "" &
+sleep 3
+
+echo "Running program..."
 exec ./replay
+
+echo "Finished running..."
